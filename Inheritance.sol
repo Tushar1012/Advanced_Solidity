2e8a7486fa2eea1088b35de1a3300efe26928d40
Tushar <Tusharsolanki1011@gmail.com>
2022-04-20 00:10:35 +0530

    done event

--- Inheritance.sol
+++ Inheritance.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
-
+//inheritance
 /* Graph of inheritance
     A
    / \
@@ -59,3 +59,4 @@ contract F is A, B {
         return super.foo();
     }
 }
+//done
